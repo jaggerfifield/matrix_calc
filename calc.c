1f445b88d1731cb276b9fbf6f96d5f26650073ca
Jagger Fifield <setUnlocalizedName@gmail.com>
2022-09-09 12:02:28 -0600

    Added some string formatting to better align matrix printing

--- calc.c
+++ calc.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define STRLEN 32
 
@@ -69,6 +70,7 @@ void main_menu(){
 char*** create_matrix(){
 	//char *m = (char*)malloc(STRLEN), *n = (char*)malloc(STRLEN);
 	int m, n;
+	unsigned int max_str_length = 0;
 
 	// Get size of array m x n
 	printf("\nEnter the size of matrix: m,n\n");
@@ -79,7 +81,8 @@ char*** create_matrix(){
 	char*** matrix = (char***)malloc(sizeof(char**) * (m + 1));
 
 	// Matrix size stored in first array
-	matrix[0] = (char**)malloc(sizeof(char*) * 2);
+	// This array is [0] = size m ; [1] = size n; [2] = largest string len
+	matrix[0] = (char**)malloc(sizeof(char*) * 3);
 	char *cm = (char*)malloc(STRLEN), *cn = (char*)malloc(STRLEN);
 	sprintf(cm, "%d", m);
 	sprintf(cn, "%d", n);
@@ -102,10 +105,20 @@ char*** create_matrix(){
 			printf("[%d][%d] = ", i - 1, j);
 			scanf("%s", l);
 
+			if(max_str_length < strlen(l)){
+				max_str_length = strlen(l);
+			}
+
 			matrix[i][j] = l;
 		}
 		printf("\n");
 	}
+
+	// Apped the max string length to the array
+	char* l = (char*)malloc(STRLEN);
+	sprintf(l, "%u", max_str_length);
+	matrix[0][2] = l;
+
 	return matrix;
 }
 
@@ -156,17 +169,27 @@ void modify_matrix(char*** matrix){
 }
 
 void print_matrix(char*** matrix){
+	printf("\n");
+
+	// Print the size of the matrix
 	printf("m = %s 	n = %s\n", matrix[0][0], matrix[0][1]);
 
 	int m, n;
 
+	// Load the size from matirx into int variables
 	sscanf(matrix[0][0], "%d", &m);
 	sscanf(matrix[0][1], "%d", &n);
 
+	// Formatting to to pass to printf, better aligns text
+	char format[STRLEN] = "[%", end[] = "s] ";
+	strcat(format, matrix[0][2]);
+	strcat(format, end);
+
 	// Print the matrix
 	for(int i = 1; i < m + 1; i++){
+		printf("R%d : ", i);
 		for(int j = 0; j < n; j++){
-			printf("[%s] ", matrix[i][j]);
+			printf(format, matrix[i][j]);
 		}
 		printf("\n");
 	}
